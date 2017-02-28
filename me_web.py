fd6292bd3dfa4cc0bfae8b2ac86a689c7f776d7b
justking100 <justking100@gmail.com>
2017-02-28 13:23:46 -0500

    fix everything

--- me_web.py
+++ me_web.py
@@ -5,12 +5,12 @@ app.debug = True
 
 @app.after_request
 def apply_caching(response):
-    response.headers['Server'] = 'AWSe'
+    response.headers['Server'] = 'AWS'
     return response
 
 @app.route("/")
 def hello():
     resp = make_response(render_template('home.html'))
-    return respada
+    return resp
 
 app.secret_key = '7\r\xec\xc0b\xad\xf4\xca\xdf\xd1\xc3\xc9\x03\xe7\xfdf\xc3\xbb\xc1\xbd\xa6+\xb2\xba'
